831561ed85092abda4d4b80dd4ee9f7c4328ea9e
Hieu Nguyen <hnnguyen45@gmail.com>
2016-12-30 19:10:13 -0800

    solved some problems 5

--- app/routes.js
+++ app/routes.js
@@ -18,7 +18,10 @@ module.exports = function(app, passport) {
         });
     });
 
-    app.delete('/')
+    app.delete('/', function(req,res){
+
+    });
+
 
 // change the bill model to have a userid property, change the insert method to set userid: req.user._id
 // change the profile to only find bills where userid: req.user._id
