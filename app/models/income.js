34125ad5f70e9c842d862815f51c2ef92017b511
Hieu Nguyen <hnnguyen45@gmail.com>
2017-01-04 00:40:49 -0800

    created new outline

--- app/models/income.js
+++ app/models/income.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 //var bcrypt   = require('bcrypt-nodejs');//Not Required
 
 // define the schema for our user model
-var billSchema = mongoose.Schema({
+var incomeSchema = mongoose.Schema({
     name: String,
     amount: Number
 });
 
-var Bill = mongoose.model('Bill', billSchema);
+var Income = mongoose.model('Income', incomeSchema);
 
 // create the model for users and expose it to our app
-module.exports = Bill;
+module.exports = Income;
